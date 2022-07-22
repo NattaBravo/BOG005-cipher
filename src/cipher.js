38631c197f1f84da4da390ba9920cca49e959a1d
NattaBravo <fiido.project@gmail.com>
2022-07-21 22:07:41 -0500

    First version

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
 const cipher = {
-  // ...
-};
+  encode: function (string, offset) { 
+    let resultado ="";
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    offset = (offset % 26 + 26) % 26;
+  
+  if (string) {
+    for (let i=0; i<string.length; i++) {
+        if (letras.indexOf(string[i])!=-1) {
+            let posicion = ((letras.indexOf(string[i])+offset)%26);
+            resultado += letras [posicion];
+        }
+        else resultado += string [i];}
+  }
+  return resultado;}
+  }
+
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+window.addEventListener("load", inicio, true);
+
+function inicio () {
+    document.getElementById("info1").addEventListener("keyup", function () {
+        this.value = this.value.toUpperCase ();
+     }, true );}
+
+document.getElementById("cipherEncode").addEventListener("click", function () {
+    let string = document.getElementById ("info1").value;
+    let offset = document.getElementById("offset").value;
+    document.getElementById("mensaje2").value = cipher.encode (string, offset);
+}, true);
+
+document.getElementById("cipherDecode").addEventListener("click", function () {
+    let string = document.getElementById ("info1").value;
+    let offset = document.getElementById("offset").value;
+    document.getElementById("mensaje2").value = cipher.encode (string, offset);
+}, true);
+
+document.getElementById("saveAnswer").addEventListener("click", function () {
+    let fecha = document.getElementById("fecha").value;
+    console.log(fecha);
+    let resultadofinal = document.getElementById("mensaje2").value;
+    console.log(resultadofinal);
+    document.getElementById("detallefecha").innerHTML = (fecha);
+    document.getElementById("note1").innerHTML = (resultadofinal);
+})
+
+    
