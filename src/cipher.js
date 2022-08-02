fd28c7b28279e949850ce40beb9e870ecc9d4db2
NattaBravo <fiido.project@gmail.com>
2022-08-02 10:46:10 -0500

    tests 100% aprobados

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,66 @@
 const cipher = {
-  
-  encode: function (string, offset) {
-    let resultado ="";
-    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    offset = (offset % 26 + 26) % 26;
-    
-    if (string) {
-    for (let i=0; i<string.length; i++) {
-        if (letras.indexOf(string[i])!=-1) {
-            let posicion = ((letras.indexOf(string[i])+offset)%26);
-            resultado += letras [posicion];
-        }}};
-    if (offset === 0 && string === 0) {
-      throw new TypeError[alert("Introduce la información requerida")]}
-    else if (offset === null || string === []) {
-      throw new TypeError}
-    else if (offset == 0 && string == 0) {
-      throw new TypeError}
-  
-  return resultado;}
+  encode: encodeFunction,
+  decode: decodeFunction
+};
 
-  ,decode: function (string, offset) { 
-    let resultado ="";
-    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    offset = (offset % 26 - 26);
-  
-  if (string) {
-    for (let i=0; i<string.length; i++) {
-        if (letras.indexOf(string[i])!=-1) {
-            let posicion = ((letras.indexOf(string[i])-offset)%26);
-            resultado += letras [posicion];
-        }
-        else resultado += string [i];}
+function encodeFunction (offset, string) {
+  if (!offset || !string) {
+    throw TypeError("Introduce la información requerida");
   }
-  return resultado;} 
+
+  let encodemessage = "";
+
+  for (let i = 0; i<string.length; i++){
+    let stringAscci = string.charCodeAt(i);
+    console.log(stringAscci);
+
+    if (stringAscci >= 65 && stringAscci <= 90) {
+      encodemessage += String.fromCharCode((stringAscci + offset - 65) % 26 + 65);
+    } else if (stringAscci >= 97 && stringAscci <= 122) {
+      encodemessage += String.fromCharCode((stringAscci + offset - 97) % 26 + 97);
+    } else {
+      encodemessage += string[i];
+    }
+  }
+return encodemessage;
 }
+
+function decodeFunction(offset, string) {
+  if (!offset || !string) {
+    throw new TypeError("Introduce la información requerida");
+  }
+
+  let decodedmessage = "";
+
+  for (let i = 0; i < string.length; i++) {
+    let stringAscci1 = string.charCodeAt(i);
+
+    if (stringAscci1 >= 65 && stringAscci1 <= 90) {
+      let criptedString = (stringAscci1 - 65 - offset % 26) % 26;
+
+      if (criptedString < 0) {
+        criptedString = criptedString + 65 + 26;
+        decodedmessage += String.fromCharCode(criptedString);
+      } else {
+        criptedString += 65;
+        decodedmessage += String.fromCharCode(criptedString);
+      }
+    } else if (stringAscci1 >= 97 && stringAscci1 <= 122) {
+      let criptedString = (stringAscci1 - 97 - offset % 26) % 26;
+
+      if (criptedString < 0) {
+        criptedString = criptedString + 97 + 26;
+        decodedmessage += String.fromCharCode(criptedString);
+      } else {
+        criptedString += 97;
+        decodedmessage += String.fromCharCode(criptedString);
+      }
+    } else {
+      decodedmessage += string[i];
+    } 
+  }
+  return decodedmessage;
+}
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
 import cipher from './cipher.js';
 
-window.addEventListener("load", inicio, true);
-
-function inicio () {
-    document.getElementById("info1").addEventListener("keyup", function () {
-        this.value = this.value.toUpperCase ();
-    }, true );}
-
 document.getElementById("cipherEncode").addEventListener("click", function () {
     let string = document.getElementById ("info1").value;
     let offset = document.getElementById("offset").value;
-    document.getElementById("mensaje2").value = cipher.encode (string, offset);
+    offset = parseInt(offset);
+    document.getElementById("mensaje2").value = cipher.encode (offset, string);
 }, true);
 
 document.getElementById("cipherDecode").addEventListener("click", function () {
     let string = document.getElementById ("info1").value;
     let offset = document.getElementById("offset").value;
-    document.getElementById("mensaje2").value = cipher.decode (string, offset);
+    document.getElementById("mensaje2").value = cipher.decode (offset, string);
 }, true);
-
-//window.addEventListener("load", viewdate, true);
-
-//function viewdate () {//
-   // document.getElementById("date1").addEventListener("keyup", function () {
-    //    let 
-    //    this.value = this.value.toUpperCase ();
-   // }, true );}//
 
 document.getElementById("saveAnswer").addEventListener("click", function () {
     let newdate = document.getElementById("date1").value;
